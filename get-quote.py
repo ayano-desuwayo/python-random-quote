7ac8adad14f1981860830731a6992e204166bcee
ayano-desuwayo <h.t.tomo@gmail.com>
2021-04-09 22:49:05 +0900

    Update get-quote.py
    random

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,16 @@
+import random
+
+
 def primary():
     print("Keep it logically awesome.")
 
-    #f = open("quotes.txt")
-    #quotes = f.readlines()
-    # f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-    # print(quotes)
+    rnd = random.randint(0, len(quotes)-1)
+
+    print(quotes[rnd])
 
 
 if __name__ == "__main__":
